f514a14a90f25e0aaa1b0fd0d2bda0faa4d98e23
bluDuckB3ar <160338513+bluDuckB3ar@users.noreply.github.com>
2024-07-21 19:45:40 -0400

    fixed meta

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,10 +1,9 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 
+const inter = new Inter({ subsets: ["latin"] });
 
-const inter = Inter({ subsets: ["latin"] });
-
-export const metadata = {
+export const data = {
   title: "Duckb3ar",
   description: "Not a quak house but a quak home",
 };
@@ -13,12 +12,12 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en" className={inter.className}>
       <head>
-        <meta name="description" content={metadata.description} />
-        <meta meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
+        <meta name="description" content={data.description} />
+        <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
         <link rel="icon" href="/duckb3ar-logo.svg" />
       </head>
       <body>
-        <div className="">          
+        <div className="">
           <div>
             {children}
           </div>
@@ -27,5 +26,4 @@ export default function RootLayout({ children }) {
     </html>
   );
 }
-
 
